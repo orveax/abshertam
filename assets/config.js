84a9c1052a3a43fb6e78d2859419dfce19958bdd
ORVEAX <orveax@gmail.com>
2026-08-21 01:38:18 +0300

    baseline: add uploaded site configuration

---
+++ assets/config.js
@@ -0,0 +1,13 @@
+window.AIBSHER_CONFIG = Object.freeze({
+  whatsappNumber: '97455117010',
+  companyProfileUrl: '',
+  locationUrl: '',
+  weatherEndpoint: '',
+  dohaTimeZone: 'Asia/Qatar',
+  operatingHours: { start: '06:30', end: '15:30' },
+  socials: {
+    facebook: '',
+    instagram: '',
+    tiktok: ''
+  }
+});
