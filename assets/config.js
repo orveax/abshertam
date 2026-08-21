be054915e55d3a8deb0dd8f739b94f7c34fad7cc
ORVEAX <orveax@gmail.com>
2026-08-21 15:14:40 +0300

    refactor: centralize operational and resource configuration

--- assets/config.js
+++ assets/config.js
@@ -1,15 +1,25 @@
 window.AIBSHER_CONFIG = Object.freeze({
+  siteUrl: 'https://abshertam.qa',
+  defaultLanguage: 'ar',
+  phoneNumber: '97455117010',
   whatsappNumber: '97455117010',
+  emails: Object.freeze({
+    general: 'info@abshertam.qa',
+    service: 'service@abshertam.qa',
+    privacy: 'privacy@abshertam.qa'
+  }),
   companyProfileUrl: '',
+  serviceBrochureUrl: '',
   locationUrl: 'https://www.google.com/maps/search/?api=1&query=8GCH%2BM7%20Doha%2C%20Qatar',
   weatherEndpoint: '',
   dohaTimeZone: 'Asia/Qatar',
-  operatingHours: { start: '06:00', end: '15:30' },
-  socials: {
+  operatingHours: Object.freeze({ start: '06:00', end: '15:30' }),
+  whatsappHours: Object.freeze({ start: '08:00', end: '20:00' }),
+  socials: Object.freeze({
     facebook: '',
     instagram: '',
     linkedin: '',
     tiktok: '',
     youtube: ''
-  }
+  })
 });
